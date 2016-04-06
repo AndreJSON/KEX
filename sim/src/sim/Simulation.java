e056e3bc08f25742ea560cad4c25112a04ee2761
Andre Josefsson <andre.v.json@gmail.com>
2016-04-06 21:32:01 +0200

    Adds a window to show the simulation in.

--- sim/src/sim/Simulation.java
+++ sim/src/sim/Simulation.java
@@ -2,8 +2,13 @@ package sim;
 
 import sim.map.track.*;
 import sim.vehicle.*;
+import javax.swing.JFrame;
 
 public class Simulation {
+	private static final int X = 0, Y = 1;
+	private static final int[] windowSize = {800, 600};
+	private JFrame window;
+
 	public Simulation() {
 		init();
 		testSquareCurveTrack();
@@ -11,11 +16,14 @@ public class Simulation {
 	}
 
 	public void init() {
-		//Do some init stuff.
+		window = new JFrame("Traffic Simulation");
+		window.setSize(windowSize[X], windowSize[Y]);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 
 	public void testSquareCurveTrack() {
-		// Testing arc.
+		//Testing arc.
 		SquareCurveTrack curve = new SquareCurveTrack(new Vector2D(0, 0), new Vector2D(0, 1), new Vector2D(1, 1));
 		System.out.println(curve);
 		System.out.println("Curvev length = " + curve.length());
