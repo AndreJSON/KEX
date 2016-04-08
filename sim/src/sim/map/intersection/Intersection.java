883f1c4785706b00ca9dbce50db37b9c05afc4af
Andre Josefsson <andre.v.json@gmail.com>
2016-04-08 16:12:15 +0200

    Adds all segments coming from east.

--- sim/src/sim/map/intersection/Intersection.java
+++ sim/src/sim/map/intersection/Intersection.java
@@ -9,11 +9,11 @@ import java.awt.Graphics2D;
 import java.util.HashMap;
 
 public class Intersection implements Drawable {
-	public static final int straight = 5, turn = 10, buffer = 1, width = 3;
+	public static final int straight = 15, turn = 10, buffer = 1, width = 3;
 	public static final int arm = straight + turn + buffer;
 	public static final int square = width * 3;
 	public static final int intersectionSize = arm * 2 + square;
-	public static final int NUMBER_OF_SEGMENTS = 9;
+	public static final int NUMBER_OF_SEGMENTS = 17;
 	private Segment[] segments; // Just holds all Segments.
 	private HashMap<Integer, Segment> startPoints; // Gives the first segment
 													// coming from each of the 4
@@ -42,7 +42,7 @@ public class Intersection implements Drawable {
 		segments[0].linkSegment(1,segments[4]);
 		segments[5] = new Segment(new SquareCurveTrack(new Vector2D(arm + width * 3 / 2, straight + turn), new Vector2D(arm + square / 2, arm + square * 4 / 5), new Vector2D(arm + square + buffer, arm + width * 5 / 2)));
 		segments[4].linkSegment(1,segments[5]);
-		segments[6] = new Segment(new LineTrack(new Vector2D(arm + square + buffer, arm + width * 5 / 2), new Vector2D(arm + square + buffer + turn, arm + width * 5 / 2)));
+		segments[6] = new Segment(new LineTrack(new Vector2D(arm + square + buffer, arm + width * 5 / 2), new Vector2D(arm * 2 + square, arm + width * 5 / 2)));
 		segments[5].linkSegment(1,segments[6]);
 		segments[6].linkSegment(1,null);
 
@@ -62,6 +62,29 @@ public class Intersection implements Drawable {
 		segments[3].linkSegment(2,null);
 
 		/***** Startpoint East *****/
+		segments[9] = new Segment(new LineTrack(new Vector2D(arm * 2 + square, arm + width / 2), new Vector2D(arm + square + buffer + turn, arm + width / 2)));
+		startPoints.put(1, segments[9]);
+
+		segments[10] = new Segment(new LineTrack(new Vector2D(arm + square + buffer + turn, arm + width / 2), new Vector2D(arm + square + buffer, arm + width / 2)));
+		segments[9].linkSegment(0,segments[10]);
+		segments[11] = new Segment(new SquareCurveTrack(new Vector2D(arm + square + buffer, arm + width / 2), new Vector2D(arm + width * 5 / 2, arm + width / 2), new Vector2D(arm + width * 5 / 2, straight + turn)));
+		segments[10].linkSegment(0,segments[11]);
+		segments[12] = new Segment(new LineTrack(new Vector2D(arm + width * 5 / 2, straight + turn), new Vector2D(arm + width * 5 / 2, 0)));
+		segments[11].linkSegment(0,segments[12]);
+		segments[12].linkSegment(0,null);
+
+		segments[13] = new Segment(new LineTrack(new Vector2D(arm + square + buffer + turn, arm + width * 3 / 2), new Vector2D(arm + square + buffer, arm + width * 3 / 2)));
+		segments[9].linkSegment(2, segments[13]);
+		segments[14] = new Segment(new SquareCurveTrack(new Vector2D(arm + square + buffer, arm + width * 3 / 2), new Vector2D(arm + square / 5, arm + square / 2), new Vector2D(arm + width / 2, arm + square + buffer)));
+		segments[13].linkSegment(2, segments[14]);
+		segments[14].linkSegment(2, segments[8]);
+		segments[8].linkSegment(2, null);
+
+		segments[15] = new Segment(new LineTrack(new Vector2D(arm + square + buffer, arm + width / 2), new Vector2D(straight + turn, arm + width / 2)));
+		segments[10].linkSegment(3, segments[15]);
+		segments[16] = new Segment(new LineTrack(new Vector2D(straight + turn, arm + width / 2), new Vector2D(0, arm + width / 2)));
+		segments[15].linkSegment(3, segments[16]);
+		segments[16].linkSegment(3, null);
 		
 	}
 
