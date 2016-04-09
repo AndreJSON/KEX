81e340181fc452989422f724b467f56c4275f45e
Andre Josefsson <andre.v.json@gmail.com>
2016-04-09 22:11:56 +0200

    Adds ability to toggle spawning from the GUI.

--- sim/src/sim/Simulation.java
+++ sim/src/sim/Simulation.java
@@ -27,10 +27,11 @@ public class Simulation implements ActionListener {
 
 	private JFrame window;
 	private SimDisplay simDisp;
-	private JButton b1;
+	private JButton b1, b2;
 	private Logic logic;
 	private AbstractTSCS tscs;
 	private int drawFps;
+	private boolean currentlySpawning = true;
 
 	/************ Just init stuff in this section *************/
 
@@ -53,6 +54,11 @@ public class Simulation implements ActionListener {
 		b1.addActionListener(this);
 		b1.setActionCommand("Brake");
 		window.add(b1);
+		b2 = new JButton("Stop spawning");
+		b2.setBounds(windowSize[Y] + 20,150, windowSize[X] - windowSize[Y] - 40,50);
+		b2.addActionListener(this);
+		b2.setActionCommand("Spawn");
+		window.add(b2);
 		window.setSize(windowSize[X], windowSize[Y]);
 		window.setLocationRelativeTo(null); // Centers window
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -93,7 +99,7 @@ public class Simulation implements ActionListener {
 				tickTime += 1e9 / TICKS_PER_SECOND;
 				spawnAccum++;
 				// Adding cars
-				if (spawnAccum >= spawnSeparation) {
+				if (spawnAccum >= spawnSeparation && currentlySpawning) {
 					spawnAccum = 0;
 					int source = (int)(Math.random() * 4);
 					int dest = (int)(Math.random() * 3 + 1) + source;
@@ -117,7 +123,12 @@ public class Simulation implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if("Brake".equals(e.getActionCommand())) {
+			currentlySpawning = false;
 			tscs.setEmergencyBreak(true);
+		}
+		else if(("Spawn").equals(e.getActionCommand())) {
+			currentlySpawning = !currentlySpawning;
+			b2.setText(currentlySpawning? "Stop spawning" : "Start spawning");
 		}
 	}
 }
